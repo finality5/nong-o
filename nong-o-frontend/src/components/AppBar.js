7ce063dc4b513126a734e1264daf21e1392cbc36
Watcharaphat Manosatiankul <watcharaphat.3166@gmail.com>
2019-04-20 15:56:05 +0700

    add AppBar, Home, styles

---
+++ nong-o-frontend/src/components/AppBar.js
@@ -0,0 +1,13 @@
+import React, {
+  memo,
+} from 'react';
+
+import MuiAppBar from '@material-ui/core/AppBar';
+
+const AppBar = memo(() => {
+  return (
+    <div>AppBar</div>
+  );
+});
+
+export default AppBar;
